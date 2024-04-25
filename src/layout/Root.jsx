469b3e9324b803eb800805e944552ca871a477ba
sajid islam <sajidisalm941@gmail.com>
2024-04-25 20:53:41 +0600

    added navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+
 const Root = () => {
+    const [darkMode,setDarkMode]=useState(false)
+    // console.log(darkMode?"true":"false")
     return (
-        <div>
+        <div className="dark">
+            <Navbar setDarkMode={setDarkMode} darkMode={darkMode}></Navbar>
             <h1>I am root</h1>
         </div>
     );
