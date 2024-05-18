98d6f5fa0d61646fc058109f7f210901ea293cb2
sajid islam <sajidisalm941@gmail.com>
2024-05-18 20:19:02 +0600

    change favion and title

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -16,7 +16,7 @@ const AuthProvider = ({ children }) => {
 
     const createUser = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
+        return createUserWithEmailAndPassword(auth,email,password)
     }
 
     const logIn = (email, password) => {
